86ce1a46394cf2482cedd94b1c25b67c250b887e
kde <kdeun1@naver.com>
2023-01-11 23:57:28 +0900

    feat: storybook의 alias 추가, sass 패키지 추가

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,3 +1,6 @@
+const path = require('path');
+const resolve = (dir) => path.join(__dirname, dir);
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -14,5 +17,13 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
-  }
+  },
+  viteFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '../src'),
+    };
+
+    return config;
+  },
 }
