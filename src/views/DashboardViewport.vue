ad620476cb2d0c8b93147ea9c100bff9f0e5b913
kde <kdeun1@naver.com>
2023-01-30 00:17:26 +0900

    feat: BaseInput 컴포넌트 웹 접근성 추가

---
+++ src/views/DashboardViewport.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import BaseNav from '@/components/baseNav/BaseNav.vue';
+import { computed, ref } from 'vue';
+import { numToPx } from '@/common/utils';
+import BaseHeader from '@/components/baseHeader/BaseHeader.vue';
+import BaseFooter from '@/components/baseFooter/BaseFooter.vue';
+
+const navPadding = ref(260);
+const navPaddingPx = computed(() => numToPx(navPadding.value));
+</script>
+
+<template>
+  <div class="dashboard-viewport">
+    <BaseNav
+      :nav-padding="navPadding"
+    />
+    <div class="layout-content">
+      <BaseHeader />
+      <main>
+        <RouterView />
+      </main>
+      <BaseFooter />
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.dashboard-viewport {
+  background-color: rgb(37, 41, 60);
+  block-size: 100%;
+}
+.layout-content {
+  display: flex;
+  flex-direction: column;
+  padding: 0 0 0 v-bind('navPaddingPx');
+}
+</style>
