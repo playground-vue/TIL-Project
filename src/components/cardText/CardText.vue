8dc5191604ea739e01fee774611618797b508453
byeongwan <byeongwan@ex-em.com>
2023-01-31 00:43:51 +0900

    feat: CardText 컴포넌트 개발

---
+++ src/components/cardText/CardText.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import BaseCard from '@/components/baseCard/BaseCard.vue';
+import BaseTitle from '@/components/baseTitle/BaseTitle.vue';
+
+interface Props {
+  title?: string;
+  subTitle?: string;
+  style?: string;
+}
+
+const props = defineProps<Props>();
+
+</script>
+
+<template>
+  <base-card class="card-text" :style="props.style">
+    <base-title>
+      {{ props.title }}
+      <template #subTitle>
+        {{ props.subTitle }}
+      </template>
+    </base-title>
+    <slot />
+  </base-card>
+</template>
+
+<style lang="scss">
+.card-text {
+  padding: 24px;
+}
+</style>
