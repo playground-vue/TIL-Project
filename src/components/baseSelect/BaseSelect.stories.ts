74651c0ffb472571af4c0e69fa9095174738ae5b
byeongwan <byeongwan@ex-em.com>
2023-01-30 20:37:46 +0900

    refactor: Base Select 컴포넌트 리팩터링

--- src/components/baseSelect/BaseSelect.stories.ts
+++ src/components/baseSelect/BaseSelect.stories.ts
@@ -14,7 +14,7 @@ const Template = (args: Props) => ({
   },
   setup() {
     const actionsData = {
-      onChangeItem: action('changeItem'),
+      onChangeItem: action('change-item'),
     };
 
     return {
@@ -22,11 +22,11 @@ const Template = (args: Props) => ({
       ...actionsData,
     };
   },
-  template: '<base-select :select-item="selectItem" v-bind="args" @changeItem="onChangeItem"/>',
+  template: '<base-select v-bind="args" @changeItem="onChangeItem"/>',
 });
 
 export const DefaultSelect: any = Template.bind({});
-const selectItem: SelectItem = { name: 'Downloaded', value: 'downloaded' };
+const modelValue: SelectItem = { name: 'Downloaded', value: 'downloaded' };
 const placeholder = 'Select';
 const items: SelectItem[] = [
   { name: 'Downloaded', value: 'downloaded' },
@@ -36,7 +36,7 @@ const items: SelectItem[] = [
   { name: 'Past Due', value: 'pastDue' },
 ];
 DefaultSelect.args = {
-  selectItem,
+  modelValue,
   placeholder,
   items,
 };
