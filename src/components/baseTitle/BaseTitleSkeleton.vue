0fbe26b2fe53203da3be3aa723b6c2250a2a869a
byeongwan <byeongwan@ex-em.com>
2023-02-01 21:24:51 +0900

    feat: BaseTitleSkelton 컴포넌트 개발

---
+++ src/components/baseTitle/BaseTitleSkeleton.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="base-title">
+    <div class="base-title__title skeleton" />
+    <div class="base-title__sub skeleton" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+.base-title {
+  display: block;
+
+  &__title {
+    width: 80%;
+    height: 24px;
+    margin-bottom: 8px;
+  }
+
+  &__sub {
+    width: 60%;
+    height: 20px;
+    border-radius: 4px;
+  }
+}
+</style>
